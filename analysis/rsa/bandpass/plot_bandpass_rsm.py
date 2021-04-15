b18babe0aa83a585dfe625e28cae0ae65f9f2c45
sousquared <sou.yoshihara.72e@gmail.com>
2021-04-15 19:06:34 +0900

    Add bandpass RSM.

---
+++ analysis/rsa/bandpass/plot_bandpass_rsm.py
@@ -0,0 +1,113 @@
+import os
+import pathlib
+import sys
+
+# add the path to load src module
+current_dir = pathlib.Path(os.path.abspath(__file__)).parent
+sys.path.append(os.path.join(str(current_dir), "../../../"))
+
+from src.analysis.rsa.utils import load_rdms
+from src.analysis.rsa.bandpass.bandpass_rsm import plot_bandpass_RSMs
+from src.model.load_sin_pretrained_models import sin_names
+
+if __name__ == "__main__":
+    arch = "alexnet"
+    num_classes = 16
+    epoch = 60
+
+    metrics = "correlation"  # ("correlation", "1-covariance", "negative-covariance")
+    analysis = f"bandpass_rdm_{metrics}"
+    new_analysis = f"bandpass_rsm_{metrics}"
+
+    in_dir = f"/Users/sou/lab1-work/blur-training-dev/analysis/rsa/bandpass/results/{analysis}/{num_classes}-class/"
+    # out_dir = f"/Users/sou/lab1-work/blur-training-dev/analysis/rsa/bandpass/plots/{new_analysis}/{num_classes}-class/"
+    out_dir = f"plots/{new_analysis}/{num_classes}-class/"
+
+    assert os.path.exists(in_dir), f"{in_dir} does not exist."
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    # models to compare
+    model_names = [
+        f"{arch}_normal",
+        f"{arch}_all_s04",
+        f"{arch}_mix_s04",
+        f"vone_{arch}",
+        sin_names[arch],
+    ]
+
+    # model_names = [
+    #     f"{arch}_normal",
+    #     # f"{arch}_multi-steps",
+    # ]
+    # modes = [
+    #     f"{arch}_all",
+    #     f"{arch}_mix",
+    #     f"{arch}_random-mix",
+    #     f"{arch}_single-step",
+    #     # f"{arch}_fixed-single-step",
+    #     # f"{arch}_reversed-single-step",
+    # ]
+    #
+    # # sigmas to compare
+    # sigmas_mix = [s for s in range(1, 6)] + [10]
+    # sigmas_random_mix = ["00-05", "00-10"]
+    #
+    # # add sigma to compare to the model names
+    # for mode in modes:
+    #     if mode == f"{arch}_random-mix":
+    #         for min_max in sigmas_random_mix:
+    #             model_names += [f"{mode}_s{min_max}"]
+    #     elif mode == f"{arch}_mix":
+    #         for sigma in sigmas_mix:
+    #             model_names += [f"{mode}_s{sigma:02d}"]
+    #     else:
+    #         for sigma in range(1, 5):
+    #             model_names += [f"{mode}_s{sigma:02d}"]
+
+    from src.analysis.rsa.rsa import alexnet_layers
+
+    min_list = []
+    max_list = []
+
+    for model_name in model_names:
+        in_file = os.path.join(in_dir, f"{analysis}_{model_name}.pkl")
+        rdms = load_rdms(file_path=in_file)
+
+        rsms = {}
+        for layer in alexnet_layers:
+            rsms[layer] = 1 - rdms[layer]
+            min_list.append((rsms[layer].min()))
+            max_list.append((rsms[layer].max()))
+
+        # get analysis parameters.
+        # num_images = rdms["num_images"]
+        num_filters = rdms["num_filters"]
+        rsms["num_filters"] = rdms["num_filters"]
+
+        # (optional) set title
+        title = f"RDM, {num_classes}-class, {model_name}"
+
+        # set filename
+        filename = f"{new_analysis}_{model_name}_f{num_filters}.png"
+        out_file = os.path.join(out_dir, filename)
+
+        # colour value range of the plots
+        vmin = -1
+        vmax = 1
+
+        # plot_rdms(rdms=diff_rdms, out_file=out_file, plot_show=True)
+        plot_bandpass_RSMs(
+            rdms=rsms,
+            num_filters=num_filters,
+            vmin=vmin,
+            vmax=vmax,
+            title=title,
+            out_file=out_file,
+            show_plot=False,
+        )
+
+    import numpy as np
+
+    print(np.array(min_list).min())
+    print(np.array(max_list).max())
---
+++ src/analysis/rsa/bandpass/bandpass_rsm.py
@@ -0,0 +1,258 @@
+import os
+import pathlib
+import sys
+
+import numpy as np
+import seaborn as sns
+import torch
+from matplotlib import pyplot as plt
+from scipy.spatial.distance import squareform, pdist
+from tqdm import tqdm
+
+# add the path to load src module
+current_dir = pathlib.Path(os.path.abspath(__file__)).parent
+sys.path.append(os.path.join(str(current_dir), "../../../../"))
+
+from src.analysis.rsa.rsa import alexnet_layers
+from src.analysis.rsa.utils import load_activations
+from src.analysis.rsa.bandpass.activations import compute_activations_with_bandpass
+
+
+def compute_bandpass_RSMs(
+    RSA,
+    data_loader: iter,
+    filters: dict,
+    add_noise: bool = True,
+    mean: float = 0.0,
+    var: float = 0.1,
+    metrics: str = "correlation",  # ("correlation", "1-covariance", "negative-covariance")
+    device: torch.device = torch.device("cuda:0"),
+) -> dict:
+    """Computes RSM for each image and return mean RSMs.
+    Args:
+        in_dir: path to input directory
+        num_filters: number of band-pass filter
+        num_images: number of images
+
+    Returns:
+        Mean RSMs (Dict)
+    """
+    mean_rsms = {}
+    mean_rsms["num_filters"] = len(filters)
+    # mean_rsms["num_images"] = len(data_loader)
+    # mean_rsms["target_id"] = target_id
+
+    for layer in tqdm(RSA.layers, desc="layers", leave=False):
+        rsms = []
+        # compute RSM for each image (with some filters applied)
+        for image_id, (image, label) in tqdm(
+            enumerate(data_loader), desc="test images", leave=False
+        ):
+            """Note that data_loader SHOULD return a single image for each loop.
+            image (torch.Tensor): torch.Size([1, C, H, W])
+            label (torch.Tensor): e.g. tensor([0])
+            """
+            activations = compute_activations_with_bandpass(
+                RSA=RSA,
+                image=image,
+                filters=filters,
+                device=device,
+                add_noise=add_noise,
+                mean=mean,
+                var=var,
+            )
+
+            # add parameter settings of this analysis
+            activations["label_id"] = label.item()
+            activations["num_filters"] = len(filters)
+
+            # save (This file size is very big with iterations!)
+            # file_name = f"image{image_id:04d}_f{len(filters):02d}.pkl"
+            # file_path = os.path.join(out_dir, file_name)
+            # save_activations(activations=activations, file_path=file_path)
+
+            # reshape activations for computing rsm
+            activation = activations[layer].reshape(len(filters) + 1, -1)
+
+            rsm = compute_RSM(activation=activation, metrics=metrics)
+
+            rsms.append(rsm)
+
+        rsms = np.array(rsms)
+
+        mean_rsms[layer] = rsms.mean(0)
+
+    return mean_rsms
+
+
+def compute_bandpass_RSMs_2(
+    RSA,
+    data_loader: iter,
+    filters: dict,
+    add_noise: bool = True,
+    mean: float = 0.0,
+    var: float = 0.1,
+    metrics: str = "correlation",  # ("correlation", "1-covariance", "negative-covariance")
+    device: torch.device = torch.device("cuda:0"),
+) -> dict:
+    """Computes RSM for each image and return mean RSMs.
+    Args:
+        in_dir: path to input directory
+        num_filters: number of band-pass filter
+        num_images: number of images
+
+    Returns:
+        Mean RSMs (Dict)
+    """
+    rsms = {}
+    for layer in RSA.layers:
+        rsms[layer] = []
+
+    # compute RSM for each image (with some filters applied)
+    for image_id, (image, label) in tqdm(
+        enumerate(data_loader), desc="test images", leave=False
+    ):
+        """Note that data_loader SHOULD return a single image for each loop.
+        image (torch.Tensor): torch.Size([1, C, H, W])
+        label (torch.Tensor): e.g. tensor([0])
+        """
+        activations = compute_activations_with_bandpass(
+            RSA=RSA,
+            image=image,
+            filters=filters,
+            device=device,
+            add_noise=add_noise,
+            mean=mean,
+            var=var,
+        )
+
+        # add parameter settings of this analysis
+        # activations["label_id"] = label.item()
+        # activations["num_filters"] = len(filters)
+
+        # save (This file size is very big with iterations!)
+        # file_name = f"image{image_id:04d}_f{len(filters):02d}.pkl"
+        # file_path = os.path.join(out_dir, file_name)
+        # save_activations(activations=activations, file_path=file_path)
+
+        for layer in RSA.layers:
+            # reshape activations for computing rsm
+            activation = activations[layer].reshape(len(filters) + 1, -1)
+
+            rsms[layer] += [compute_RSM(activation=activation, metrics=metrics)]
+
+    mean_rsms = {}
+    mean_rsms["num_filters"] = len(filters)
+    # mean_rsms["num_images"] = len(data_loader)
+    # mean_rsms["target_id"] = target_id
+    for layer in RSA.layers:
+        all_rsms = np.array(rsms[layer])
+
+        mean_rsms[layer] = all_rsms.mean(0)
+
+    return mean_rsms
+
+
+def compute_RSM(activation, metrics):
+    """Computes RSM.
+    Args:
+        activation: (N, dim_features)
+            N: number of stimuli
+            dim_features: dimension of features (e.g. height x width of a feature map)
+        metrics: ("correlation", "covariance")
+    Returns: RSM (N, N)
+    """
+    if metrics == "correlation":
+        rsm = 1 - squareform(
+            pdist(activation, metric=metrics)
+        )  # 1 - (1 - corr.) = corr.
+    elif metrics == "covariance":
+        rsm = squareform(
+            pdist(
+                activation,
+                lambda u, v: np.average((u - np.average(u)) * (v - np.average(v))),
+            )  # cov.
+        )  # TODO: compute diagonal coefficients (They are zeros after passed to "squareform()")
+
+    return rsm
+
+
+def plot_bandpass_RSMs(
+    rsms,
+    layers=alexnet_layers,
+    num_filters=6,
+    vmin=0,
+    vmax=2,
+    title="",
+    out_file="rsms.png",
+    show_plot=False,
+):
+    """Plot several layers RSMs in one figure.
+    Args:
+        model_name: name of the model to examine.
+    """
+    fig = plt.figure(dpi=300)
+
+    for i, layer in enumerate(layers):
+        ax = fig.add_subplot(2, 4, i + 1)
+        # sns.set(font_scale=0.5)  # adjust the font size of labels
+        ax.set_title(layer)
+
+        sns.heatmap(
+            rsms[layer],
+            ax=ax,
+            square=True,
+            vmin=vmin,
+            vmax=vmax,
+            xticklabels=["0", "0-1", "1-2", "2-4", "4-8", "8-16", "16-"],
+            yticklabels=["0", "0-1", "1-2", "2-4", "4-8", "8-16", "16-"],
+            cmap="coolwarm",
+            cbar=False,
+            # --- show values ---
+            # annot=True,
+            # fmt="1.2f",
+            # annot_kws={'size': 3}
+            # ---  ---
+            # cbar_ax=cbar_ax,  # show color bar
+        )
+
+        ax.hlines(
+            [i for i in range(2, num_filters + 1)],
+            *ax.get_xlim(),
+            linewidth=0.1,
+            colors="gray",
+        )
+        ax.vlines(
+            [i for i in range(2, num_filters + 1)],
+            *ax.get_ylim(),
+            linewidth=0.1,
+            colors="gray",
+        )
+        ax.hlines(
+            1,  # diff. line for separating raw images and bandpass images
+            *ax.get_xlim(),
+            linewidth=1,
+            colors="gray",
+        )
+        ax.vlines(
+            1,  # diff. line for separating raw images and bandpass images
+            *ax.get_xlim(),
+            linewidth=1,
+            colors="gray",
+        )
+
+    # show color bar
+    # cbar_ax = fig.add_axes([.91, .3, .03, .4])
+    # sns.heatmap(rsms[layer], cbar=True, cbar_ax=cbar_ax,
+    #             vmin=-1, vmax=1, cmap='coolwarm',
+    #             xticklabels=False, yticklabels=False)
+
+    # sns.set(font_scale=0.5)  # adjust the font size of title
+    if title:
+        fig.suptitle(title)
+    # fig.tight_layout(rect=[0, 0, 0.9, 1])
+    fig.tight_layout()
+    plt.savefig(out_file)
+    if show_plot:
+        plt.show()
+    plt.close()
