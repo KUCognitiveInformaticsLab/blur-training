1afa4222bd5e95a36fff73493557b34c84dea21f
sousquared <sou.yoshihara.72e@gmail.com>
2021-04-21 19:50:11 +0900

    Add untrained model.

--- analysis/rsa/bandpass/compute_plot_bandpass_rsm.py
+++ analysis/rsa/bandpass/compute_plot_bandpass_rsm.py
@@ -64,11 +64,11 @@ if __name__ == "__main__":
 
     # models to compare
     model_names = [
-        "alexnet_normal",
-        "alexnet_all_s04",
-        "alexnet_mix_s04",
-        sin_names[arch],
-        "vone_alexnet",
+        # "alexnet_normal",
+        # "alexnet_all_s04",
+        # "alexnet_mix_s04",
+        # sin_names[arch],
+        # "vone_alexnet",
         "untrained_alexnet",
     ]
     # model_names = [
