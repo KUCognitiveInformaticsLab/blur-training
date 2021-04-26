ed46000f86776d30681f6059234ba741bc6f2d28
sousquared <sou.yoshihara.72e@gmail.com>
2021-04-26 17:00:28 +0900

    Add utils.

--- analysis/rsa/bandpass/plot_bandpass_rsm_graph.py
+++ analysis/rsa/bandpass/plot_bandpass_rsm_graph.py
@@ -12,7 +12,7 @@ from src.analysis.rsa.utils import load_rsms
 from src.analysis.rsa.bandpass.bandpass_rsm_graph import compute_bandpass_values
 from src.model.load_sin_pretrained_models import sin_names
 from src.analysis.rsa.rsa import alexnet_layers, vone_alexnet_layers
-
+from src.analysis.plot import colors, lines
 
 if __name__ == "__main__":
     arch = "alexnet"
@@ -22,9 +22,9 @@ if __name__ == "__main__":
     metrics = "correlation"  # ("correlation", "1-covariance", "negative-covariance")
     analysis = f"bandpass_rsm_{metrics}"
 
-    # in_dir = f"/Users/sou/lab1-work/blur-training-dev/analysis/rsa/bandpass/results/{analysis}/{num_classes}-class/"
+    in_dir = f"/Users/sou/lab1-work/blur-training-dev/analysis/rsa/bandpass/results/{analysis}/{num_classes}-class/"
     # out_dir = f"/Users/sou/lab1-work/blur-training-dev/analysis/rsa/bandpass/plots/{analysis}/{num_classes}-class/"
-    in_dir = f"./results/{analysis}/{num_classes}-class/"
+    # in_dir = f"./results/{analysis}/{num_classes}-class/"
     out_dir = f"./plots/{analysis}_graph/{num_classes}-class/"
 
     assert os.path.exists(in_dir), f"{in_dir} does not exist."
@@ -41,70 +41,13 @@ if __name__ == "__main__":
         sin_names[arch],
     ]
 
-    # model_names = [
-    #     f"{arch}_normal",
-    #     # f"{arch}_multi-steps",
-    # ]
-    # modes = [
-    #     f"{arch}_all",
-    #     f"{arch}_mix",
-    #     f"{arch}_random-mix",
-    #     f"{arch}_single-step",
-    #     # f"{arch}_fixed-single-step",
-    #     # f"{arch}_reversed-single-step",
-    # ]
-    #
-    # # sigmas to compare
-    # sigmas_mix = [s for s in range(1, 6)] + [10]
-    # sigmas_random_mix = ["00-05", "00-10"]
-    #
-    # # add sigma to compare to the model names
-    # for mode in modes:
-    #     if mode == f"{arch}_random-mix":
-    #         for min_max in sigmas_random_mix:
-    #             model_names += [f"{mode}_s{min_max}"]
-    #     elif mode == f"{arch}_mix":
-    #         for sigma in sigmas_mix:
-    #             model_names += [f"{mode}_s{sigma:02d}"]
-    #     else:
-    #         for sigma in range(1, 5):
-    #             model_names += [f"{mode}_s{sigma:02d}"]
-
-    colors = {
-        f"untrained_{arch}": "gray",
-        f"{arch}_normal": "#1f77b4",
-        f"{arch}_all_s04": "darkorange",
-        f"{arch}_mix_s04": "limegreen",
-        f"{arch}_mix_s10": "hotpink",
-        f"{arch}_random-mix_s00-05": "green",
-        f"{arch}_random-mix_s00-10": "mediumvioletred",
-        f"{arch}_trained_on_SIN": "mediumvioletred",
-        f"vone_{arch}": "blueviolet",
-    }
-
-    lines = {
-        f"untrained_{arch}": ":",
-        f"{arch}_normal": ":",
-        f"{arch}_all_s04": "-",
-        f"{arch}_mix_s04": "-",
-        f"{arch}_mix_s10": "-",
-        f"{arch}_random-mix_s00-05": "-",
-        f"{arch}_random-mix_s00-10": "-",
-        f"{arch}_trained_on_SIN": "-",
-        f"vone_{arch}": "-",
-    }
-
     # (optional) set title
     plot_title = f"{analysis}, {num_classes}-class"
 
     # set filename
     num_filters = 6
-    filename = f"{analysis}_graph_{num_classes}-class_f{num_filters}.png"
+    filename = f"{analysis}_graph-one-side_{num_classes}-class_f{num_filters}.png"
     out_file = os.path.join(out_dir, filename)
-
-    a = ((1, 2), (2, 1), (2, 3), (3, 2), (3, 4), (4, 3), (4, 5), (5, 4), (5, 6), (6, 5))
-    b = ((1, 3), (2, 4), (3, 1), (3, 5), (4, 2), (4, 6), (5, 3), (6, 4))
-    c = ((1, 4), (2, 5), (3, 6), (4, 1), (5, 2), (6, 3))
 
     x = [1, 2, 3]
     fig = plt.figure(dpi=300)
