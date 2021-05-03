8dad7cc0692d55c157e63997a56c06a8e430307e
sousquared <sou.yoshihara.72e@gmail.com>
2021-05-03 15:58:04 +0900

    t-SNE.

--- analysis/rsa/bandpass/compute_plot_tSNE.sh
+++ analysis/rsa/bandpass/compute_plot_tSNE.sh
@@ -1,32 +1,32 @@
-python compute_plot_tSNE.py \
-  --num_classes 1000 \
-  --compute False \
-  --num_dim 2 \
-  --perplexity 30 
-  
-python compute_plot_tSNE.py \
-  --num_classes 1000 \
-  --compute False \
-  --num_dim 2 \
-  --perplexity 10
-  
-python compute_plot_tSNE.py \
-  --num_classes 1000 \
-  --compute False \
-  --num_dim 2 \
-  --perplexity 20 
-  
-python compute_plot_tSNE.py \
-  --num_classes 1000 \
-  --compute False \
-  --num_dim 2 \
-  --perplexity 40 
-  
-python compute_plot_tSNE.py \
-  --num_classes 1000 \
-  --compute False \
-  --num_dim 2 \
-  --perplexity 50 
+#python compute_plot_tSNE.py \
+#  --num_classes 1000 \
+#  --compute False \
+#  --num_dim 2 \
+#  --perplexity 30
+#
+#python compute_plot_tSNE.py \
+#  --num_classes 1000 \
+#  --compute False \
+#  --num_dim 2 \
+#  --perplexity 10
+#
+#python compute_plot_tSNE.py \
+#  --num_classes 1000 \
+#  --compute False \
+#  --num_dim 2 \
+#  --perplexity 20
+#
+#python compute_plot_tSNE.py \
+#  --num_classes 1000 \
+#  --compute False \
+#  --num_dim 2 \
+#  --perplexity 40
+#
+#python compute_plot_tSNE.py \
+#  --num_classes 1000 \
+#  --compute False \
+#  --num_dim 2 \
+#  --perplexity 50
 
 ## dim = 3
 python compute_plot_tSNE.py \
