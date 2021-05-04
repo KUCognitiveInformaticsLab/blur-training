abbefc01665dea4d75185ec1d1501201c3bde4b9
sousquared <sou.yoshihara.72e@gmail.com>
2021-05-04 22:09:22 +0900

    lowpass acc.

--- src/utils/accuracy.py
+++ src/utils/accuracy.py
@@ -53,6 +53,8 @@ def accuracy(output, target, topk=(1,)):
         pred = pred.t()
         correct = pred.eq(target.view(1, -1).expand_as(pred))
 
+        print(correct.shape)
+
         res = []
         for k in topk:
             correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
