3f0b67c4f83ac892d142dab19094a33da12632e4
sousquared <sou.yoshihara.72e@gmail.com>
2021-06-11 17:42:31 +0900

    Add new training.

--- train_imagenet16_vone_alexnet_0-gpu1.sh
+++ train_imagenet16_vone_alexnet_0-gpu1.sh
@@ -1,8 +1,9 @@
 # mix
 python src/train_imagenet16.py --log_dir /mnt/data/pretrained_models/blur-training/imagenet16 --arch vone_alexnet --mode mix -s 4 -n vone_alexnet_mix_s04
+# multi-steps
+python src/train_imagenet16.py --log_dir /mnt/data/pretrained_models/blur-training/imagenet16 --arch vone_alexnet --mode multi-steps -n vone_alexnet_multi-steps
+
+# mix
 python src/train_imagenet16.py --log_dir /mnt/data/pretrained_models/blur-training/imagenet16 --arch vone_alexnet --mode mix -s 1 -n vone_alexnet_mix_s01
 python src/train_imagenet16.py --log_dir /mnt/data/pretrained_models/blur-training/imagenet16 --arch vone_alexnet --mode mix -s 2 -n vone_alexnet_mix_s02
 python src/train_imagenet16.py --log_dir /mnt/data/pretrained_models/blur-training/imagenet16 --arch vone_alexnet --mode mix -s 3 -n vone_alexnet_mix_s03
-
-# multi-steps
-python src/train_imagenet16.py --log_dir /mnt/data/pretrained_models/blur-training/imagenet16 --arch vone_alexnet --mode multi-steps -n vone_alexnet_multi-steps
