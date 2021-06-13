49f2f80af055fef34c0d4bc2609e52e93cb916e8
sousquared <sou.yoshihara.72e@gmail.com>
2021-06-13 17:06:56 +0900

    Add new training.

--- train_imagenet16_vone_alexnet_0-gpu1.sh
+++ train_imagenet16_vone_alexnet_0-gpu1.sh
@@ -1,5 +1,5 @@
 # all
-python src/train_imagenet16.py --log_dir /mnt/data/pretrained_models/blur-training/imagenet16 --mode all -s 4 -n vone_alexnet_all_s04
+#python src/train_imagenet16.py --log_dir /mnt/data/pretrained_models/blur-training/imagenet16 --arch vone_alexnet --mode all -s 4 -n vone_alexnet_all_s04
 
 # mix
 python src/train_imagenet16.py --log_dir /mnt/data/pretrained_models/blur-training/imagenet16 --arch vone_alexnet --mode mix -s 4 -n vone_alexnet_mix_s04
--- train_imagenet16_vone_alexnet_0.sh
+++ train_imagenet16_vone_alexnet_0.sh
@@ -2,10 +2,10 @@
 python src/train_imagenet16.py --log_dir /mnt/data/pretrained_models/blur-training/imagenet16 --arch vone_alexnet --mode normal -n vone_alexnet_normal
 
 # all
-python src/train_imagenet16.py --log_dir /mnt/data/pretrained_models/blur-training/imagenet16 --mode all -s 1 -n vone_alexnet_all_s01
-python src/train_imagenet16.py --log_dir /mnt/data/pretrained_models/blur-training/imagenet16 --mode all -s 2 -n vone_alexnet_all_s02
-python src/train_imagenet16.py --log_dir /mnt/data/pretrained_models/blur-training/imagenet16 --mode all -s 3 -n vone_alexnet_all_s03
-python src/train_imagenet16.py --log_dir /mnt/data/pretrained_models/blur-training/imagenet16 --mode all -s 4 -n vone_alexnet_all_s04
+#python src/train_imagenet16.py --log_dir /mnt/data/pretrained_models/blur-training/imagenet16 --arch vone_alexnet --mode all -s 1 -n vone_alexnet_all_s01
+#python src/train_imagenet16.py --log_dir /mnt/data/pretrained_models/blur-training/imagenet16 --arch vone_alexnet --mode all -s 2 -n vone_alexnet_all_s02
+#python src/train_imagenet16.py --log_dir /mnt/data/pretrained_models/blur-training/imagenet16 --arch vone_alexnet --mode all -s 3 -n vone_alexnet_all_s03
+#python src/train_imagenet16.py --log_dir /mnt/data/pretrained_models/blur-training/imagenet16 --arch vone_alexnet --mode all -s 4 -n vone_alexnet_all_s04
 
 # mix
 python src/train_imagenet16.py --log_dir /mnt/data/pretrained_models/blur-training/imagenet16 --arch vone_alexnet --mode mix -s 1 -n vone_alexnet_mix_s01
