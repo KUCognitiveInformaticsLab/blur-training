f637bd6eba9117547af4d7493229255b3ed71a2b
sousquared <sou.yoshihara.72e@gmail.com>
2021-06-14 06:34:04 +0900

    random-mix ex.

--- train_imagenet16_vone_alexnet_gpu1_random-mix-ex.sh
+++ train_imagenet16_vone_alexnet_gpu1_random-mix-ex.sh
@@ -1,4 +1,4 @@
-# random-mix
+# random-mix-ex
 python src/train_imagenet16.py --log_dir /mnt/data/pretrained_models/blur-training/imagenet16 --arch vone_alexnet --mode random-mix --min_sigma 0 --max_sigma 4 -n vone_alexnet_random-mix_s00-04_no-blur-1label --excluded_labels 15;
 python src/train_imagenet16.py --log_dir /mnt/data/pretrained_models/blur-training/imagenet16 --arch vone_alexnet --mode random-mix --min_sigma 0 --max_sigma 4 -n vone_alexnet_random-mix_s00-04_no-blur-8label --excluded_labels 8 9 10 11 12 13 14 15;
 python src/train_imagenet16.py --log_dir /mnt/data/pretrained_models/blur-training/imagenet16 --arch vone_alexnet --mode random-mix --min_sigma 0 --max_sigma 2 -n vone_alexnet_random-mix_s00-02_no-blur-8label --excluded_labels 8 9 10 11 12 13 14 15;
