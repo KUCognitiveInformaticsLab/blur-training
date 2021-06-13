fc08ffb8ab83a0283fe2cb55913f11d662b1c67c
sousquared <sou.yoshihara.72e@gmail.com>
2021-06-13 16:46:23 +0900

    lowpass acc.

--- analysis/lowpass_acc/compute_lowpass_acc.py
+++ analysis/lowpass_acc/compute_lowpass_acc.py
@@ -80,7 +80,7 @@ if __name__ == "__main__":
 
     model_names = [
         # f"untrained_{arch}",
-        f"{arch}_normal",
+        # f"{arch}_normal",
         f"{arch}_all_s04",
         f"{arch}_mix_s04",
         f"{arch}_multi-steps",
