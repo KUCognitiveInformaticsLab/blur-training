c18853ef1fe883ee9b37f5750ff6822fa5a6aff5
sousquared <sou.yoshihara.72e@gmail.com>
2021-06-14 16:11:58 +0900

    all-training

--- analysis/run/all-blur-training_vonenet.sh
+++ analysis/run/all-blur-training_vonenet.sh
@@ -1,6 +1,6 @@
-cd ../lowpass_acc
-python compute_lowpass_acc.py vone_alexnet 16 imagenet16 all-blur-training
-python compute_plot_lowpass_confusion_matrix.py vone_alexnet 16 imagenet16 all-blur-training
+#cd ../lowpass_acc
+#python compute_lowpass_acc.py vone_alexnet 16 imagenet16 all-blur-training
+#python compute_plot_lowpass_confusion_matrix.py vone_alexnet 16 imagenet16 all-blur-training
 
 cd ../shape-bias
 python compute_shape_bias.py vone_alexnet 16 all-blur-training
@@ -19,9 +19,9 @@ python compute_plot_confusion_matrix_and_acc_on_jumbled_images.py vone_alexnet 1
 python compute_plot_confusion_matrix_and_acc_on_jumbled_images.py vone_alexnet 16 imagenet16 jumbled_with_gray_occluder 16 all-blur-training
 python compute_plot_confusion_matrix_and_acc_on_jumbled_images.py vone_alexnet 16 imagenet16 jumbled_with_gray_occluder 32 all-blur-training
 
-cd ../bandpass_acc
-python compute_bandpass_acc.py vone_alexnet 16 imagenet16 all-blur-training
-python compute_bandpass_confusion_matrix.py vone_alexnet 16 imagenet16 all-blur-training
-
-cd ../rsa/bandpass/rsm
-python compute_plot_bandpass_rsm.py vone_alexnet 16 all-blur-training
+#cd ../bandpass_acc
+#python compute_bandpass_acc.py vone_alexnet 16 imagenet16 all-blur-training
+#python compute_bandpass_confusion_matrix.py vone_alexnet 16 imagenet16 all-blur-training
+#
+#cd ../rsa/bandpass/rsm
+#python compute_plot_bandpass_rsm.py vone_alexnet 16 all-blur-training
