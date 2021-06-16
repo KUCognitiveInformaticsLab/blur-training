b74e31c27e3e9cc820674768e4dc16eb413e1da1
sousquared <sou.yoshihara.72e@gmail.com>
2021-06-16 11:15:19 +0900

    no-label

---
+++ analysis/run/no-label.sh
@@ -0,0 +1,17 @@
+cd ../lowpass_acc
+#python compute_lowpass_acc.py vone_alexnet 16 imagenet16 vss
+python compute_plot_lowpass_confusion_matrix.py vone_alexnet 16 imagenet16 mix_no-blur gpu1
+python compute_plot_lowpass_confusion_matrix.py vone_alexnet 16 imagenet16 mix_no-sharp gpu1
+python compute_plot_lowpass_confusion_matrix.py vone_alexnet 16 imagenet16 random-mix_no-blur gpu1
+python compute_plot_lowpass_confusion_matrix.py vone_alexnet 16 imagenet16 random-mix_no-sharp gpu1
+python compute_plot_lowpass_confusion_matrix.py alexnet 16 imagenet16 mix_no-blur gpu1
+python compute_plot_lowpass_confusion_matrix.py alexnet 16 imagenet16 mix_no-sharp gpu1
+python compute_plot_lowpass_confusion_matrix.py alexnet 16 imagenet16 random-mix_no-blur gpu1
+python compute_plot_lowpass_confusion_matrix.py alexnet 16 imagenet16 random-mix_no-sharp gpu1
+
+#cd ../bandpass_acc
+#python compute_bandpass_acc.py vone_alexnet 16 imagenet16 vss gpu1
+#python compute_bandpass_confusion_matrix.py vone_alexnet 16 imagenet16 vss gpu1
+
+#cd ../rsa/bandpass/rsm
+#python compute_plot_bandpass_rsm.py vone_alexnet 16 vss gpu1
