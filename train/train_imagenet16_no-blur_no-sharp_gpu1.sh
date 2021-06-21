085323cb29b9be194f4b76447843ebd32085de8e
sousquared <sou.yoshihara.72e@gmail.com>
2021-06-21 16:30:43 +0900

    no-label training

--- train/train_imagenet16_no-blur_no-sharp_gpu1.sh
+++ train/train_imagenet16_no-blur_no-sharp_gpu1.sh
@@ -1,9 +1,9 @@
 # mix_no-sharp seed0
-python ../src/train_imagenet16.py --seed 0 -n vone_alexnet_mix_s04_no-sharp-1label_seed0 --log_dir /mnt/data/pretrained_models/sharp-training/imagenet16 --mode mix_no-sharp --arch vone_alexnet -s 4 --excluded_labels 15;
-python ../src/train_imagenet16.py --seed 0 -n vone_alexnet_mix_s04_no-sharp-8label_seed0 --log_dir /mnt/data/pretrained_models/sharp-training/imagenet16 --mode mix_no-sharp --arch vone_alexnet -s 4 --excluded_labels 8 9 10 11 12 13 14 15;
+python ../src/train_imagenet16.py --seed 0 -n vone_alexnet_mix_s04_no-sharp-1label_seed0 --log_dir /mnt/data/pretrained_models/blur-training/imagenet16 --mode mix_no-sharp --arch vone_alexnet -s 4 --excluded_labels 15;
+python ../src/train_imagenet16.py --seed 0 -n vone_alexnet_mix_s04_no-sharp-8label_seed0 --log_dir /mnt/data/pretrained_models/blur-training/imagenet16 --mode mix_no-sharp --arch vone_alexnet -s 4 --excluded_labels 8 9 10 11 12 13 14 15;
 
-python ../src/train_imagenet16.py --seed 0 -n alexnet_mix_s04_no-sharp-1label_seed0 --log_dir /mnt/data/pretrained_models/sharp-training/imagenet16 --mode mix_no-sharp --arch alexnet -s 4 --excluded_labels 15;
-python ../src/train_imagenet16.py --seed 0 -n alexnet_mix_s04_no-sharp-8label_seed0 --log_dir /mnt/data/pretrained_models/sharp-training/imagenet16 --mode mix_no-sharp --arch alexnet -s 4 --excluded_labels 8 9 10 11 12 13 14 15;
+python ../src/train_imagenet16.py --seed 0 -n alexnet_mix_s04_no-sharp-1label_seed0 --log_dir /mnt/data/pretrained_models/blur-training/imagenet16 --mode mix_no-sharp --arch alexnet -s 4 --excluded_labels 15;
+python ../src/train_imagenet16.py --seed 0 -n alexnet_mix_s04_no-sharp-8label_seed0 --log_dir /mnt/data/pretrained_models/blur-training/imagenet16 --mode mix_no-sharp --arch alexnet -s 4 --excluded_labels 8 9 10 11 12 13 14 15;
 
 # mix_no-blur alexnet seed0
 python ../src/train_imagenet16.py --seed 0 -n alexnet_mix_s04_no-blur-1label_seed0 --log_dir /mnt/data/pretrained_models/blur-training/imagenet16 --mode mix --arch alexnet -s 4 --excluded_labels 15; 
