1827b8f113da585ecb799174d5609b357358c7c5
sousquared <sou.yoshihara.72e@gmail.com>
2021-06-23 16:45:18 +0900

    Add a training run-script.

--- train/train_imagenet1000_vone_alexnet_gpu1.sh
+++ train/train_imagenet1000_vone_alexnet_gpu1.sh
@@ -1,3 +1,3 @@
 # mix, all (s=04)
-python src/train_imagenet1000.py --log_dir /mnt/data/pretrained_models/blur-training/imagenet1000 --arch vone_alexnet --mode mix -s 4 -n vone_alexnet_mix_s04
-python src/train_imagenet1000.py --log_dir /mnt/data/pretrained_models/blur-training/imagenet1000 --arch vone_alexnet --mode all -s 4 -n vone_alexnet_all_s04
+python ../src/train_imagenet1000.py --log_dir /mnt/data/pretrained_models/blur-training/imagenet1000 --arch vone_alexnet --mode mix -s 4 -n vone_alexnet_mix_s04
+python ../src/train_imagenet1000.py --log_dir /mnt/data/pretrained_models/blur-training/imagenet1000 --arch vone_alexnet --mode all -s 4 -n vone_alexnet_all_s04
