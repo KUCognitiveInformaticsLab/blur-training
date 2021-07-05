d74858224e71b99b41e43c3942cf388be44d16ea
sousquared <sou.yoshihara.72e@gmail.com>
2021-07-05 17:00:09 +0900

    dist.

--- analysis/rsa/dist/compute_plot_dist_sample.sh
+++ analysis/rsa/dist/compute_plot_dist_sample.sh
@@ -1,3 +1,7 @@
+python compute_plot_dist.py --server gpu2 --compute --plot -a alexnet --stimuli s-b --models vss
+python compute_plot_dist.py --server gpu2 --compute --plot -a alexnet --stimuli h-l --models vss
+python compute_plot_dist.py --server gpu2 --compute --plot -a alexnet --stimuli h-l --models test
+
 python compute_plot_dist.py --server gpu2 --compute --plot -a alexnet --models mix_no-blur
 python compute_plot_dist.py --server gpu2 --compute --plot -a alexnet --models mix_no-sharp
 
